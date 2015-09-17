8d25cc2d956b43001eca0f4edab60fba9edf198b
jinghewang <jinghewang@163.com>
2015-09-17 17:11:53 +0800

    redo

--- student.rb
+++ student.rb
@@ -3,11 +3,11 @@ class Student
 
 
   puts '-------if---------'
-  # 单行if
+  # 鍗曡if
   x = 3 ; y = 6
   (printf '%s < %s',x,y) if x < y
 
-  # 多行if
+  # 澶氳if
   x = 3 ; y = 6
   msg = ''
   if x < y then
@@ -30,11 +30,11 @@ class Student
   end
 
   puts '-------while---------'
-  # 单行 while
+  # 鍗曡 while
   x = 3
   (puts x; x += 1;) while x < 8
 
-  # 多行 while
+  # 澶氳 while
   x = 1 ; y =10
   while x < 10
     puts x
@@ -49,11 +49,80 @@ class Student
   end
 
 
+  puts '-------for unless---------'
+  list = ['wjh', 'lvkui', 'wangjingzhi']
+  for x in list
+    unless x == 'wjh' then
+      puts x
+    end
+  end
+
+  puts '-------for if not---------'
+  for x in list
+    if not x == 'lvkui' then
+      puts x
+    end
+  end
+
+  puts '-------for 2------------------'
+  for x in 1..6
+      puts x
+  end
+
+
+  puts '-------for 3------------------'
+  dosth = dosth2 = false
+  for x in 1..5
+    for y in 1..5
+      if x == 2 and y == 4 then
+        next
+      end
+      if x == 4 and y == 4 then
+        break
+      end
+      printf "[%s:%s]", x, y
+      if x == 5 and y == 4 and !dosth then
+        dosth = true
+        redo
+      end
+      if x == 1 and y == 5 and !dosth2 then
+        # dosth2 = true
+        #  retry
+      end
+    end
+    puts ''
+  end
+
+
+  puts "------婕旂ずredo----"
+  c='a'
+  for i in 1..4
+    if i == 2 and c =='a'
+      c = 'b'
+      print "\n"
+      redo
+    end
+    print i,c," "
+  end
+  puts "\n\n"
+
+
+  puts "------婕旂ずretry----"
+  c = 'a'
+  for i in 1..4
+    if i == 2 and c == 'a' then
+      c = 'b'
+      redo
+    end
+    puts i,c," "
+  end
 
 
 
   exit
 
+
+  puts '-----------------------------------'
 
   a = 1;b = 2;
   puts a + b
