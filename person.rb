6f7a1beafd3fadb69c11cee6569c492bacf05062
jinghewang <jinghewang@163.com>
2015-09-19 23:55:22 +0800

    public protected private

--- person.rb
+++ person.rb
@@ -2,44 +2,29 @@ class Person
 
   @@count = 0;
 
-  $global_name = 'hlt'
-
   def initialize(name , age =18)
-    @name = name
-    @age = age
-    @motherland = "China"
-    @addr = ""
-
-    @@count += 1
   end
-
-  def self.instance_count()
-    return @@count
-  end
-
-  # getter and setter 方式
-  def addr=(value)
-    return @addr =value
-  end
-
-  def addr
-    return @addr
-  end
-
-  # getter and setter 方式2
-  attr_writer :motherland
 
   def talk
-    puts "my name is:" + @name + " and age is :" + @age.to_s() + " and my addr is:" + @addr
-    if @motherland == "China" then
-      puts "I am chinese"
-    else
-      puts "I am foreigner"
-    end
+    puts "public:talk ,call speak"
+    speak
   end
 
-  def notalk()
-
+  def speak
+    puts "protected:speak, call laugh"
+    laugh
   end
+
+  def laugh
+    puts "private:langh"
+  end
+
+  protected :speak
+  private :laugh
 
 end
+
+p1 = Person.new("wjh")
+p1.talk
+p1.speak
+p1.langh
