4e3a6cc9cb247a76a998f64f39b0e19331e08c5c
jinghewang <jinghewang@163.com>
2015-09-21 22:32:55 +0800

    fruit mod

--- fruit/fruit.rb
+++ fruit/fruit.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.expand_path('../../base',__FILE__)) unless $LOAD_PATH.include?(File.expand_path('../../base',__FILE__))
 $LOAD_PATH.unshift(File.expand_path('../../modules',__FILE__)) unless $LOAD_PATH.include?(File.expand_path('../../modules',__FILE__))
-$LOAD_PATH.unshift(File.expand_path('../../fruit',__FILE__)) unless $LOAD_PATH.include?(File.expand_path('../../fruit',__FILE__))
+$LOAD_PATH.unshift(File.expand_path('../../f1',__FILE__)) unless $LOAD_PATH.include?(File.expand_path('../../f1',__FILE__))
 
 require 'me'
+require 'mod'
 
 class Fruit
 
@@ -14,6 +15,12 @@ class Fruit
   end
 end
 
-fruit = Fruit.new
-puts fruit.get_fruit_name
-puts fruit.hello
+
+
+f1 = Fruit.new
+f2 = Fruit.new
+puts f1.get_fruit_name
+puts f1.hello
+f2.extend(Mod)
+f2.hello2
+# f1.hello2 # error used
---
+++ modules/mod.rb
@@ -0,0 +1,9 @@
+
+
+module Mod
+
+  def hello2(name='wjh')
+    puts "hello2:#{name}"
+  end
+
+end
